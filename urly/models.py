98753ce4c0aa60ff93186a1b6dcd79f6796941cc
Donato <donato.gr@gmail.com>
2016-10-09 22:21:53 +0200

    modelli

--- urly/models.py
+++ urly/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Url(models.Model):
-	short = models.CharField(_('Short url'), maxlength=255, unique=True)
-	url = models.CharField(_('Original url'), maxlength=2048, unique=True)
-	
+    short = models.CharField(_('Short url'), maxlength=255, primary=True)
+    url = models.CharField(_('Original url'), maxlength=2048, db_index=True)
+    
