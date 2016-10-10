586ecfc90d0f06fa6700bc8b4e5ceaae4b7c8c43
Donato <donato.gr@gmail.com>
2016-10-10 07:58:04 +0200

    test

--- urly/views.py
+++ urly/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from urly.models import Url
+from django.views.generic.base import RedirectView
 
 # Create your views here.
-def redirect(request, path):
-	try:
-		
+class Redirect(RedirectView):
+	
+	def get(self, path):
+		short = get_object_or_404(Url, short=path).values_list('short', flat=True)[0]
+		return short
